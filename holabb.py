7780321f32ed76df53b9e6c4bbcd53c8e1bb19bf
Jaime Jesús <zx21student017@colegiovalledelmiro.es>
2023-02-15 13:40:00 +0100

    15/02

---
+++ holabb.py
@@ -0,0 +1,5 @@
+#!C:\Users\zx21student017\AppData\Local\Microsoft\WindowsApps\python
+
+print("Content-Type: text/html\n")
+
+print("<!DOCTYPE html><html><head></head><body><h1>hola bb</h1></body></html>")
